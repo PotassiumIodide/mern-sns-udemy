83ecf21656e1bec389d9df7cc183a0007d26016a
PotassiumIodide <potassium.iodide28@gmail.com>
2022-08-31 17:35:58 +0900

    Add: styles for Rightbar component

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -6,18 +6,25 @@ export default function Rightbar() {
     <div className="rightbar">
       <div className="rightbarWrapper">
         <div className="eventContainer">
-          <img src="assets/star.png" alt="" className="startImg" />
+          <img src="assets/star.png" alt="" className="starImg" />
           <span className="eventText"><b>フォロワー限定</b>イベント開催中</span>
         </div>
-        <img src="assets/event.jpeg" alt="" />
-        <h4 className="rightbarTittle">オンラインの友達</h4>
+        <img src="assets/event.jpeg" alt="" className="eventImg" />
+        <h4 className="rightbarTitle">オンラインの友達</h4>
         <ul className="rightbarFriendList">
           <li className="rightbarFriend">
             <div className="rightbarProfileImgContainer">
-              <img src="assets/person/1.jpeg" alt="" />
+              <img src="assets/person/1.jpeg" alt="" className="rightbarProfileImg" />
               <span className="rightbarOnline"></span>
             </div>
             <span className="rightbarUsername">Shin Code</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Sato</span>
           </li>
         </ul>
       </div>
