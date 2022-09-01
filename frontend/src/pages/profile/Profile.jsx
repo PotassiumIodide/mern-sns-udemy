4205718f46f8f1767ddab1f88596cb61a6978b74
PotassiumIodide <potassium.iodide28@gmail.com>
2022-09-01 13:50:56 +0900

    Fix: img file path in profile page

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -6,30 +6,31 @@ import Topbar from '../../components/topbar/Topbar';
 import "./Profile.css";
 
 function Profile() {
-    return (
-        <>
-            <Topbar />
-            <div className="profile">
-                    <Sidebar />
-                    <div className="profileRight">
-                        <div className="profileRightTop">
-                            <div className="profileCover">
-                                <img src="assets/post/3.jpeg" alt="" className="profileCoverImg" />
-                                <img src="assets/person/1.jpeg" alt="" className="profileUserImg" />
-                            </div>
-                            <div className="profileInfo">
-                                <h4 className="profileInfoName">Shin Code</h4>
-                                <span className="profileInfoDesc">Udemy講師をしています</span>
-                            </div>
-                            <div className="profileRightBottom">
-                                <TimeLine />
-                                <Rightbar profile />
-                            </div>
-                        </div>
-                    </div>
+  const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img src={PUBLIC_FOLDER + "/post/3.jpeg"} alt="" className="profileCoverImg" />
+              <img src={PUBLIC_FOLDER + "/person/1.jpeg"} alt="" className="profileUserImg" />
             </div>
-        </>
-    )
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Shin Code</h4>
+              <span className="profileInfoDesc">Udemy講師をしています</span>
+            </div>
+            <div className="profileRightBottom">
+              <TimeLine />
+              <Rightbar profile />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Profile
