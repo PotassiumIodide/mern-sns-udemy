a7f22bf023f6a37e80150b83129555ce58ac172b
PotassiumIodide <potassium.iodide28@gmail.com>
2022-09-01 13:21:04 +0900

    Add: Register component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Home from "./pages/home/Home";
 import Profile from './pages/profile/Profile';
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
-  return <Login />;
+  return <Register />;
 }
 
 export default App;
