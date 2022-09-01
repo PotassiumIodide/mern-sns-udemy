6ad348aca98096ef39c4b306e62c055ae21e9a01
PotassiumIodide <potassium.iodide28@gmail.com>
2022-09-01 13:03:30 +0900

    Add: implement left part of Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Home from "./pages/home/Home";
 import Profile from './pages/profile/Profile';
+import Login from "./pages/login/Login";
 
 function App() {
-  return <Profile />;
+  return <Login />;
 }
 
 export default App;
---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+        <div className="loginWrapper">
+            <div className="loginLeft">
+                <h3 className="loginLogo">MERN SNS</h3>
+                <span className="loginDesc">本格的なSNSを、自分の手で。</span>
+            </div>
+            <div className="loginRight">
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
