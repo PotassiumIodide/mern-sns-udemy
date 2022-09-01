79cb63351536cd9b5fdbd2973c8bc4858e41764b
PotassiumIodide <potassium.iodide28@gmail.com>
2022-09-01 16:18:00 +0900

    Update: get user info using query

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import Rightbar from '../../components/rightbar/Rightbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import TimeLine from '../../components/timeline/TimeLine';
@@ -7,6 +8,18 @@ import "./Profile.css";
 
 function Profile() {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await axios.get(`/users?username=PotassiumIodide`);
+      console.log(response);
+      setUser(response.data);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <>
       <Topbar />
@@ -19,12 +32,12 @@ function Profile() {
               <img src={PUBLIC_FOLDER + "/person/1.jpeg"} alt="" className="profileUserImg" />
             </div>
             <div className="profileInfo">
-              <h4 className="profileInfoName">Shin Code</h4>
-              <span className="profileInfoDesc">Udemy講師をしています</span>
+              <h4 className="profileInfoName">{user.username}</h4>
+              <span className="profileInfoDesc">{user.desc}</span>
             </div>
             <div className="profileRightBottom">
               <TimeLine username="PotassiumIodide" />
-              <Rightbar profile />
+              <Rightbar user={user} />
             </div>
           </div>
         </div>
