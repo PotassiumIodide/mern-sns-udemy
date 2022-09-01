74d5bed5e8287068d4233929309a7dfb02fa6129
PotassiumIodide <potassium.iodide28@gmail.com>
2022-09-01 14:37:14 +0900

    Update: replace dummy data with fetched data from mongo DB

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,12 +1,24 @@
 import { MoreVert } from '@mui/icons-material';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Post.css";
-import { Users } from "../../dummyData";
+// import { Users } from "../../dummyData";
+import axios from "axios";
 
 export default function Post({ post }) {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
   const [like, setLike] = useState(post.like);
   const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await axios.get(`/users/${post.userId}`);
+      console.log(response);
+      setUser(response.data);
+    };
+    fetchUser();
+  }, []);
+
   const handleLike = () => {
       setLike(isLiked ? like - 1 : like + 1);
       setIsLiked(!isLiked)
@@ -17,11 +29,11 @@ export default function Post({ post }) {
         <div className="postTop">
           <div className="postTopLeft">
             <img src={
-                PUBLIC_FOLDER + Users.filter((user) => user.id === post.id)[0].profilePicture
+              user.profilePicture
             } alt="" className="postProfileImg"
             />
             <span className="postUsername">
-            {Users.filter((user) => user.id === post.id)[0].username}
+              {user.username}
             </span>
             <span className="postDate">{post.date}</span>
           </div>
