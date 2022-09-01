74aced449d93b357ab7c03e1e0507bce45611576
PotassiumIodide <potassium.iodide28@gmail.com>
2022-09-01 16:47:17 +0900

    Update: get values of email and password using useRef Hooks

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useRef } from 'react';
 import "./Login.css";
 
-function Login() {
+function Login() {  
+  const email = useRef();
+  const password = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email.current.value);
+    console.log(password.current.value);
+  };
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -10,14 +20,27 @@ function Login() {
           <span className="loginDesc">本格的なSNSを、自分の手で。</span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
+          <form className="loginBox" onSubmit={(e) => handleSubmit(e)}>
             <p className="loginMsg">ログインはこちら</p>
-            <input type="email" className="loginInput" placeholder="Eメール" required />
-            <input type="password" className="loginInput" placeholder="パスワード" required minLength="6" />
+            <input
+              type="email"
+              className="loginInput"
+              placeholder="Eメール"
+              required
+              ref={email}
+            />
+            <input
+              type="password"
+              className="loginInput"
+              placeholder="パスワード"
+              required
+              minLength="6"
+              ref={password}
+            />
             <button className="loginButton">ログイン</button>
             <span className="loginForgot">パスワードを忘れた方へ</span>
             <button className="loginRegisterButton">アカウント作成</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
