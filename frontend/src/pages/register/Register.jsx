d79cf64fdbf463afd3532da45a9d5d24d728b7f0
PotassiumIodide <potassium.iodide28@gmail.com>
2022-09-01 17:47:56 +0900

    Update: Register.jsx

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useRef } from 'react';
 import "./Register.css";
 
 function Register() {
+  const username= useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordConfirmation = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="register">
       <div className="registerWrapper">
@@ -10,15 +19,37 @@ function Register() {
           <span className="registerDesc">本格的なSNSを、自分の手で。</span>
         </div>
         <div className="registerRight">
-          <div className="registerBox">
+          <form className="registerBox" onSubmit={(e) => handleSubmit(e)}>
             <p className="registerMsg">新規登録はこちら</p>
-            <input type="text" className="registerInput" placeholder="Eメール" />
-            <input type="text" className="registerInput" placeholder="パスワード" />
-            <input type="text" className="registerInput" placeholder="ユーザー名" />
-            <input type="text" className="registerInput" placeholder="確認用パスワード" />
-            <button className="registerButton">サインアップ</button>
+            <input
+              type="text"
+              className="registerInput"
+              placeholder="ユーザー名"
+              required
+            />
+            <input
+              type="email"
+              className="registerInput"
+              placeholder="Eメール"
+              required
+            />
+            <input
+              type="password"
+              className="registerInput"
+              placeholder="パスワード"
+              required
+              minLength="6"
+            />
+            <input
+              type="password"
+              className="registerInput"
+              placeholder="確認用パスワード"
+              required
+              minLength="6"
+            />
+            <button className="registerButton" type="submit">サインアップ</button>
             <button className="registerRegisterButton">ログイン</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
